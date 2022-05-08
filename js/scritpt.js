3ecd5541ccfc749ff41da70d5d35f01536a960eb
willyongaks <william.ongaki10@gmail.com>
2022-05-08 11:49:42 +0200

    b

--- js/scritpt.js
+++ js/scritpt.js
@@ -1,5 +1,5 @@
-
-let favourites = [];
+const listKey = "list"
+let favourites = retrieveFromStorage();
 let books = [
     {
         isbn: "1600506460320",
@@ -26,58 +26,58 @@ const favButton = document.querySelector("button");
 
 
 
-// function createList() {
+function createList() {
 
-//     listHtml.innerHTML = "";
+    listHtml.innerHTML = "";
 
-//     books.forEach(function (book) {
+    books.forEach(function (book) {
 
-//     listHtml.innerHTML += `
-//                             <li class="list_item"> Title : ${book.title} <br><br>
-//                              isbn : ${book.isbn} <i class="fa fa-trash" aria-hidden="true" data-item="${book.isbn}"></i> </li>
+    listHtml.innerHTML += `
+                            <li class="list_item"> Title : ${book.title} <br><br>
+                             isbn : ${book.isbn} <i class="fa fa-trash" aria-hidden="true" data-item="${book.isbn}"></i> </li>
 
-//                              `
-//     });
-
-
-//     const removeBtn = document.querySelectorAll("li i");
+                             `
+    });
 
 
-//     removeBtn.forEach(function (btn) {
-
-//         btn.addEventListener("click", removeItem);
-//     });
-// };
-
-// createList();
+    const removeBtn = document.querySelectorAll("li i");
 
 
+    removeBtn.forEach(function (btn) {
+
+        btn.addEventListener("click", removeItem);
+    });
+};
+
+createList();
 
 
-// function removeItem(event) {
-//     // console.log(event)
 
-//     const removeBook = event.target.dataset.item;
 
-//     const newBookList = books.filter(function (item) {
-//         if (removeBook !== item.isbn) {
-//             return true;
-//         }
+function removeItem(event) {
+    // console.log(event)
 
-//     });
-//     // console.log(item)
+    const removeBook = event.target.dataset.item;
 
-//     books = newBookList;
+    const newBookList = books.filter(function (item) {
+        if (removeBook !== item.isbn) {
+            return true;
+        }
 
-//     createList();
+    });
+    // console.log(item)
 
-// };
+    books = newBookList;
+
+    createList();
+
+};
 
 
 
 
 // Creating favourite book items
-
+createFavs();
 
 favButton.addEventListener("click", addBookList);
 
@@ -90,7 +90,9 @@ function addBookList() {
     if (newBook.length >= 2) {
         favourites.push(newBook);
         createFavs();
+        saveToStorage(favourites);
         listInput.value = "";
+        listInput.focus();
     }
 }
 
@@ -101,11 +103,45 @@ function createFavs() {
 
     favourites.forEach(function (bookItem) {
         listContainer.innerHTML += ` <li>${bookItem} <button data-id="${bookItem}">Remove book</button></li>`
-    })
+    });
+
+    const items = document.querySelectorAll("li button");
+
+    items.forEach(function(bookItem) {
+        bookItem.addEventListener("click", removeBookList);
+    });
+}
+
+function removeBookList(event) {
+    console.log(event)
+    const deleteBook = event.target.dataset.id;
+
+    const newBookList = favourites.filter(function(bookItem) {
+        if(deleteBook !== bookItem) {
+            return true;
+        }
+        
+    });
+console.log(favourites)
+    favourites = newBookList;
+
+    createFavs();
 }
 
 
+function saveToStorage(bookToSave){
+    localStorage.setItem(listKey , JSON.stringify(bookToSave));
+}
 
+function retrieveFromStorage() {
+    const currentFavs = localStorage.getItem(listKey);
+
+    if(!currentFavs) {
+        return [];
+    }
+
+    return JSON.parse(currentFavs);
+}
 
 
 
